e6dbb18b4b44e1256871cd38a8ad7a42e2d81182
nduartech <nduartech101@gmail.com>
2024-09-29 02:14:33 -0400

    Tweaks

--- dot_config/eww/executable_get-gpu-vram.sh
+++ dot_config/eww/executable_get-gpu-vram.sh
@@ -16,9 +16,9 @@ vram_usage=$((vram_used * 100 / vram_total))
 
 # Output icon based on GPU usage
 if [ $gpu_usage -ge 80 ]; then
-    echo "󰢮 $gpu_usage% | 󰍛 $vram_usage% "  # High GPU usage icon
+    echo "📹 $gpu_usage% | 🐐 $vram_usage% "  # High GPU usage icon
 elif [ $gpu_usage -ge 50 ]; then
-    echo "󰢮 $gpu_usage% | 󰍛 $vram_usage% "  # Medium GPU usage icon
+    echo "📹 $gpu_usage% | 🐐 $vram_usage% "  # Medium GPU usage icon
 else
-    echo "󰢮 $gpu_usage% | 󰍛 $vram_usage% "  # Low GPU usage icon
+    echo "📹 $gpu_usage% | 🐐 $vram_usage% "  # Low GPU usage icon
 fi
